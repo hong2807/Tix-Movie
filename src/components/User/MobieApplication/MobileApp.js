73f57f814bfc06753ddaf12c7bf64686ca4f2190
Hong Huynh <honghtt@devblock.net>
2020-11-09 06:07:57 +0700

    update ui 09.11.2020

---
+++ src/components/User/MobieApplication/MobileApp.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Slider from "react-slick";
+import './MobileApp.scss'
+
+export default function MobileApp() {
+    // Slick
+    const settings = {
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 2,
+          slidesToScroll: 1
+    };
+
+    return (
+        <div className="mobileappComponent">
+            <div className="container">
+                <div className="mobileapp__content">
+                    <div className="row">
+                        <div className="col-12 col-md-12 col-lg-6">
+                            <div className="mobileapp__left">
+                                <h2>Ứng dụng tiện lợi dành cho người yêu điện ảnh</h2>
+                                <br />
+                                <p>Không chỉ đặt vé, bạn còn có thể bình luận phim, chấm điểm rạp và đổi quà hấp dẫn.</p>
+                                <br />
+                                <button className="btn btn-danger">App miễn phí - Tải về ngay!</button>
+                                <p className="textApp">
+                                TIX có hai phiên bản
+                                <a className="version" target="_blank" href="https://itunes.apple.com/us/app/123phim-mua-ve-lien-tay-chon/id615186197?mt=8">
+                                    iOS
+                                </a>
+                                &amp;
+                                <a className="version" target="_blank" href="https://play.google.com/store/apps/details?id=vn.com.vng.phim123">
+                                    Android
+                                </a>
+                                </p>
+                            </div>
+                            </div>
+                        <div className="col-12 col-md-12 col-lg-6">
+                            <div className="mobileapp__right">
+                                {/* <img className="mobileapp-mobile" src="/images/mobile.png" alt /> */}
+                                <div className="mobileapp__slider">
+         
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Slider {...settings}>
+                    <div classname="item">
+                        {/* <img src="/images/mobile1.jpg" alt="mobile1" /> */}
+                        1
+                    </div>
+                    <div classname="item">
+                        {/* <img src="/images/mobile2.jpg" alt="mobile1" /> */}
+                        2
+                    </div>
+                    <div classname="item">
+                        {/* <img src="/images/mobile3.jpg" alt="mobile1" /> */}
+                        3
+                    </div>
+                </Slider>
+            </div>
+        </div>
+    );
+}
--- src/pages/User/Home/Home.js
+++ src/pages/User/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Banner from '../../../components/User/Banner/Banner'
 import Cinema from '../../../components/User/Cinema/Cinema'
 import Films from '../../../components/User/Films/Films'
-import Footer from '../../../components/Share/Footer/Footer';
-
+import Footer from '../../../components/Share/Footer/Footer'
+import MobileApp from '../../../components/User/MobieApplication/MobileApp'
 import './Home.scss'
 
 export default function Home() {
@@ -12,6 +12,7 @@ export default function Home() {
             <Banner></Banner>
             <Films></Films>
             <Cinema></Cinema>
+            <MobileApp></MobileApp>
             <Footer></Footer>
         </div>
     )
